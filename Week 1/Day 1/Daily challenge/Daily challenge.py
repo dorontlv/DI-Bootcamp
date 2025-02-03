fe664f77fc6ad660591e145a8dd098bf1d80b3c7
dorontlv <doron.seminar@gmail.com>
2025-02-03 09:27:53 +0200

    Week 2 - Day 1

--- Week 1/Day 1/Daily challenge/Daily challenge.py
+++ Week 1/Day 1/Daily challenge/Daily challenge.py
@@ -2,11 +2,6 @@
 Daily challenge: Build up a string
 
 
-What You will learn:
-Python Basics
-Conditionals
-Loops
-
 """
 
 print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -14,9 +9,6 @@ print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 1.
 Using the input function, ask the user for a string. The string must be 10 characters long.
-If it’s less than 10 characters, print a message which states “string not long enough”.
-If it’s more than 10 characters, print a message which states “string too long”.
-If it’s 10 characters, print a message which states “perfect string” and continue the exercise.
 """
 
 my_string = input("1. Please enter a 10 chars long string: ")
@@ -32,10 +24,6 @@ print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """ 
 2.
 Then, print the first and last characters of the given text.
-The user enters "HelloWorld"
-Then you have to print 
-H
-d
 """
 
 my_string = input("2. Please enter a sentence: ")
@@ -46,15 +34,8 @@ print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """ 
 3.
-Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
+Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed.
 
-The user enters "HelloWorld"
-Then, you have to construct the string character by character
-H
-He
-Hel
-... etc
-HelloWorld
 """
 
 my_string = input("3. Please enter a sentence: ")
@@ -72,7 +53,6 @@ print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 4.
 Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method). For example:
 
-Hlrolelwod
 """
 
 import random
