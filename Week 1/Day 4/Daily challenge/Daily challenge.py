989701847186c80237d863f4a465202d8c1ddd4f
dorontlv <doron.seminar@gmail.com>
2025-02-06 10:43:13 +0200

    Week 1 - Day 4

--- Week 1/Day 4/Daily challenge/Daily challenge.py
+++ Week 1/Day 4/Daily challenge/Daily challenge.py
@@ -21,37 +21,52 @@ Using his technique, try to decode this matrix.
 
 '''
 
-Matrix = [
-['7','i','i'],
-['T','s','x'],
-['h','%','?'],
-['i',' ','#'],
-['s','M',' '],
-['$','a',' '],
-['#','t','%'],
-['^','r','!']
-]
+encrypted_string_with_newline = '''7ii
+Tsx
+h%?
+i #
+sM 
+$a 
+#t%
+^r!
+'''
 
-matrix_string = ""
+encrypted_string = ""
+
+# just to remove the newline in the string (although maybe there is a function in python that can do this)
+for chr in encrypted_string_with_newline:
+    if chr != "\n":
+        encrypted_string += chr
+
+print("This is the matrix string:")
+print (encrypted_string)
+
+Matrix = []
+index = 0
+
+# put the string into a matrix
+for line in range(8):
+    Matrix.append([])
+    for column in range(3):
+        Matrix[line].append(encrypted_string[index])
+        index += 1
+
+print("This is the matrix:")
+print (Matrix)
+
 matrix_string_decrypted_message = ""
 letter = False
 
-# go over the matrix - read the matrix message
-for line in range(len(Matrix[0])):
-    for column in range(len(Matrix)):
-        matrix_string += Matrix[column][line]
-
-print(f"The matrix message is: {matrix_string}")
-
 # decrypt the message
-for chr in matrix_string:
-    if chr>='a' and chr<='z' or chr>='A' and chr<='Z':
-        matrix_string_decrypted_message += chr
-        letter = True
-    elif letter:
+for line in range(len(Matrix[0])):  # 3
+    for column in range(len(Matrix)):  # 8
+        chr = Matrix[column][line]
+        if chr>='a' and chr<='z' or chr>='A' and chr<='Z':
+            matrix_string_decrypted_message += chr
+            letter = True
+        elif letter:
             matrix_string_decrypted_message += ' '
             letter = False
-
-print(f"The matrix decrypted message is: {matrix_string_decrypted_message}")
-
+ 
+print(f"The matrix decrypted message is:\n{matrix_string_decrypted_message}")
 
